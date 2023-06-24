e3f59f45100fd9f30054f9dac85b810a20e510dd
midia-hamdi <midiahamdi367@gmail.com>
2023-06-24 19:03:21 +0330

    main

--- src/components/clock.jsx
+++ src/components/clock.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Clock extends Component {
+    constructor (props){
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
   render() {
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-function render(){
-    ReactDOM.render(<App />, document.getElementById('root'))
-}
-
-setInterval(render, 1000)
+ReactDOM.render(<App />, document.getElementById('root'))
