0d0fd8959fc896664264880cbbfa41054e108d21
midia-hamdi <midiahamdi367@gmail.com>
2023-06-24 18:18:12 +0330

    main

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-scripts';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './app';
 
 ReactDOM.render(<App />, document.getElementById('root'))
